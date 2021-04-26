0ea0779052f57a083e9969551d536499155322c7
maruf-ahmed <ahmedmaruf474@gmail.com>
2021-04-26 16:23:16 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import Grid from "./Grid";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const randomColor = () => {
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            const random = Math.random();
+            const bit = (random * 16) | 0;
+            color += (bit).toString(16);
+        }
+        return color;
+    };
+
+    let plattes = []
+    let [ ranColor, changeColor ] = useState( randomColor() )
+    let change = ()=> changeColor(randomColor())
+    for (let i=1; i<6;i++) plattes.push(<Grid color={ranColor} opacity={0.2*i}/>)
+    return (
+        <div className="App">
+            <h1>Random Hex color Generator</h1>
+            <button onClick={change}>Generate</button>
+            <div className="platte">
+                {plattes}
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Grid.jsx
@@ -0,0 +1,11 @@
+
+
+function Grid(props){
+    // console.log(props)
+    return (
+        <div className={'grid'} style={{backgroundColor:props.color, opacity:props.opacity}}>
+        </div>
+    )
+}
+
+export default Grid
